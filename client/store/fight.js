6d0f88e0c7ae2828cd2ef8b4dba0057c03b91dc0
hbkop <h.kopenski@gmail.com>
2018-03-20 16:59:26 -0400

    renders battleground, working on socket emission

---
+++ client/store/fight.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+//initial state
+const fight = {opponentSocketId: ''};
+
+//action types
+const GET_OPPONENT = 'GET_OPPONENT';
+
+//action creators
+const getOpponent = opponent => ({type: GET_OPPONENT, opponent});
+
+//thunks
+export function fetchOpponent(id){
+  return function thunk(dispatch){
+    return axios.get(`/api/fights/${id}`)
+    .then(res => res.data)
+    .then(opponent => dispatch(getOpponent(opponent)))
+    .catch(err => console.log(err));
+  }
+}
+
+//reducer
+export default function reducer(state = fight, action){
+  switch (action.type) {
+    case GET_OPPONENT:
+      return Object.assign({}, state, {opponentSocketId: action.opponent.socketId} )
+    default: 
+      return state
+  }
+}

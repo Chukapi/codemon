d4ce3ae2b131cc0addd4270afa3f1acbfe4802b2
hbkop <h.kopenski@gmail.com>
2018-03-26 17:35:03 -0400

    battle modal

--- client/components/battle-modal.js
+++ client/components/battle-modal.js
@@ -1,22 +1,33 @@
 import React, {Component} from 'react';
 import Modal from 'react-modal';
 import {connect} from 'react-redux';
+import {setCurrentPokemon} from '../store'
 Modal.setAppElement("#app")
 
 class BattleModal extends Component {
+  
+  handleClick = (evt, pokeId) => {
+    this.props.setCurrent(pokeId)
+  }
 
   render(){
     console.log('IN BATTLE MODAL', this.props)
     if(this.props.showBattleModal === false){
       return null;
     }
+
     let modalStyle = {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      zIndex: '9999',
-      background: '#fff'
+      content : {
+        position: 'absolute',        
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: '9999',
+        backgroundColor: 'rgba(255,255,255)'          
+      }
     }
 
     let backdropStyle = {
@@ -29,19 +40,33 @@ class BattleModal extends Component {
       background: 'rgba(0, 0, 0, 0.3)'
     }
       return (
-        <div>
+        <div style={backdropStyle}>
           <Modal isOpen={this.props.showBattleModal} style={modalStyle}>
             <h2>{this.props.msg}</h2>
+            <p>Select one of your Pokemon and click "Battle!" to accept your opponent's challenge</p>
+            {this.props.pokemons.map(pokemon => {
+              return (
+                  <img className="poke-images" key={pokemon.id} onClick={(evt) => this.handleClick(evt, pokemon.id)} src={pokemon.imageUrl} />
+              )
+            })}
+            <div>
+              <button>Battle!</button>
+              <button>Decline</button>
+            </div>
           </Modal>
-          <div style={backdropStyle}/>
         </div>
       )
   }
 }
 
 const mapState = (state) => ({
+  pokemons: state.user.pokemons,
   showBattleModal: state.battleModal.isOpen, 
   msg: state.battleModal.msg
+})
+
+const mapDispatch = (dispatch) => ({
+  setCurrent: (poke) => dispatch(setCurrentPokemon(poke))
 })
 
 export default connect(mapState)(BattleModal)

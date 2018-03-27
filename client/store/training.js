8be3eee26af74fbb8a1f886d9538495a27d3235d
hbkop <h.kopenski@gmail.com>
2018-03-27 14:29:16 -0400

    commit before pull

--- client/store/training.js
+++ client/store/training.js
@@ -6,29 +6,16 @@ const currentProblem = {}
 const GET_ONE_PROBLEM = 'GET_ONE_PROBLEM'
 
 //action creators
-<<<<<<< HEAD
 const oneProblem = problem => ({type: GET_ONE_PROBLEM, problem})
-=======
-// const allProblems = problems => ({ type: ALL_PROBLEMS, problems })
-const oneProblem = problem => ({ type: GET_ONE_PROBLEM, problem })
->>>>>>> master
 
 //thunks
 
 export function fetchOneProblem(userId) {
   return function thunk(dispatch) {
     return axios.get(`/api/training/${userId}`)
-<<<<<<< HEAD
     .then(res => res.data)
     .then(problem => dispatch(oneProblem(problem)))
     .catch(err => console.log(err))
-=======
-      .then(res => res.data)
-      .then(problem => {
-        dispatch(oneProblem(problem))
-      })
-      .catch(err => console.log(err))
->>>>>>> master
   }
 }
 

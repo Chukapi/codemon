fcdb4cd28d753a9ef5b9e59d0b3cafd37ac31a83
Johanna Fulghum <johanna.fulghum@gmail.com>
2018-03-28 16:32:00 -0400

    Update battle.js

--- client/components/battle.js
+++ client/components/battle.js
@@ -13,7 +13,6 @@ class Battle extends Component {
 
   render(){
     const {pokemons, prepare } = this.props;
-    console.log(prepare, "PREPARE")
     return (
       <div>
         { prepare === null ? <div><img src="https://i.ytimg.com/vi/n03yTW2_ZSw/maxresdefault.jpg" /> </div> :

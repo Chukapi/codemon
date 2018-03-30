5110156f89e5ae4e9212eb48147f531bad42ef68
Daymis Vargas <daymis.vargas@gmail.com>
2018-03-30 10:57:48 -0400

    added exact path

--- client/routes.js
+++ client/routes.js
@@ -30,6 +30,7 @@ class Routes extends Component {
           isLoggedIn &&
           <Switch>
             {/* Routes placed here are only available after logging in */}
+            <Route exact path="/" component={UserHome} />
             <Route path="/home" component={UserHome} />
             <Route path="/mystats/:id" component={UserStats} />
             <Route path="/fights/:id" component={Battle} />
